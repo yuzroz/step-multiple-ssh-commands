e76e25c105660f6ba9d2698707bfa43cb5dded18
Jasper van Wanrooy <jasper@vanwanrooy.net>
2014-09-09 00:29:28 +0200

    First attempt to run multiple remote ssh commands.

---
+++ run.sh
@@ -0,0 +1,50 @@
+if [ ! -n "$WERCKER_MULTIPLE_SSH_COMMANDS_DEPLOY_USER" ] ; then
+    error "deploy-user property is not set or empty."
+    exit 1
+fi
+
+if [ ! -n "$WERCKER_MULTIPLE_SSH_COMMANDS_DEPLOY_HOST" ] ; then
+    error "deploy-host property is not set or empty."
+    exit 1
+fi
+
+if [ ! -n "$WERCKER_MULTIPLE_SSH_COMMANDS_PROXY_VARS" ] ; then
+    error "proxy-vars property is not set or empty."
+    exit 1
+fi
+
+if [ ! -n "$WERCKER_MULTIPLE_SSH_COMMANDS_COMMANDS" ] ; then
+    error "commands property is not set or empty."
+    exit 1
+fi
+
+##
+# Include vars to the SSH command.
+# borrowed from: https://github.com/anka-sirota/step-script-ssh/
+##
+ENV=''
+
+for f in $WERCKER_MULTIPLE_SSH_COMMANDS_PROXY_VARS
+do
+    var=$f;
+    ENV+=" export $f='${!var}';"
+done
+echo $ENV
+
+##
+# Extract the commands from the property and combine them.
+##
+COMMANDS=''
+
+$IFS="
+"
+
+for c in $WERCKER_MULTIPLE_SSH_COMMANDS_COMMANDS
+do
+    COMMANDS+="$c && "
+done
+COMMANDS=${COMMANDS:(-4)}
+
+
+ssh $DEPLOY_USER@$DEPLOY_HOST $ENV $COMMANDS
+
