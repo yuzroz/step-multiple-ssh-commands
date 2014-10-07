1cd11c30912a7ba54e6e3af5dc5b58b664001132
Jasper van Wanrooy <jasper@vanwanrooy.net>
2014-10-07 18:34:52 +0200

    Fix issue which replaces _DOLLAR_ in the command with $.
    Unfortunately the commands string is included in a new string. The $ doesn't get escaped. However, when escaping the $, it gets a double escape and so $ is still unescaped.

--- run.sh
+++ run.sh
@@ -29,6 +29,13 @@ for f in $WERCKER_MULTIPLE_SSH_COMMANDS_PROXY_VARS ; do
     ENV+="export $f='${!var}'; "
 done
 
+
+##
+# Wercker automatically escapes $ signs. However, this makes this step unusable
+# since it parses it as a local variable. This needs to be replaced...
+##
+COMMANDS_SRC="${WERCKER_MULTIPLE_SSH_COMMANDS_COMMANDS/_DOLLAR_/\$}"
+
 ##
 # Extract the commands from the property and combine them.
 # Each line in the option is a command, but do test whether it is not empty.
@@ -38,7 +45,7 @@ COMMANDS=''
 IFS="
 "
 
-for c in $WERCKER_MULTIPLE_SSH_COMMANDS_COMMANDS ; do
+for c in $COMMANDS_SRC ; do
     if [ -n "$c" ] ; then
         COMMANDS+="$c && "
         info "including command: $c"
